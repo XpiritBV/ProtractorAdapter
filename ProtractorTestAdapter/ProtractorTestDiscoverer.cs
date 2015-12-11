01840c49ec27e2e1350277dc424621fcf12b4d71
Marcel de Vries <vriesmarcel@hotmail.com>
2015-12-11 15:01:26 +0100

    initial commit

---
+++ ProtractorTestAdapter/ProtractorTestDiscoverer.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
+
+namespace ProtractorTestAdapter
+{
+    [FileExtension(".js")]
+    [DefaultExecutorUri(ProtractorTestExecutor.ExecutorUriString)]
+    public class ProtractorTestDiscoverer : ITestDiscoverer
+    {
+        public void DiscoverTests(IEnumerable<string> sources, IDiscoveryContext discoveryContext, IMessageLogger logger,
+            ITestCaseDiscoverySink discoverySink)
+        {
+            foreach (var source in sources)
+            {
+                Debugger.Break();
+                logger.SendMessage(TestMessageLevel.Error, source);
+            }
+
+            GetTests(sources, discoverySink);
+        }
+
+        internal static IEnumerable<TestCase> GetTests(IEnumerable<string> sources, ITestCaseDiscoverySink discoverySink)
+        {
+            var tests = new List<TestCase>();
+
+            foreach (string source in sources)
+            {
+                string TestName = GetTestNameFromFile(source);
+                var testCase = new TestCase(TestName, ProtractorTestExecutor.ExecutorUri, source);
+                        tests.Add(testCase);
+
+                if (discoverySink != null)
+                {
+                    discoverySink.SendTestCase(testCase);
+                }
+
+            }
+            return tests;
+        }
+
+        private static string GetTestNameFromFile(string source)
+        {
+            var testNames = new List<string>();
+            if (File.Exists(source))
+            {
+
+                using (var stream = File.OpenRead(source))
+                {
+                    using (var textReader = new StreamReader(stream))
+                    {
+                        while (!textReader.EndOfStream)
+                        {
+                            var resultLine = textReader.ReadLine();
+                            if (resultLine != null && resultLine.Contains("describe('"))
+                            {
+                                var name = GetNameFromDescribeLine(resultLine);
+                                testNames.Add(name);
+                            }
+                        }
+                    }
+                    stream.Close();
+                }
+            }
+            return string.Join(", ", testNames);
+        }
+
+        private static string GetNameFromDescribeLine(string resultLine)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ProtractorTestAdapter/ProtractorTestExecutor.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
+
+namespace ProtractorTestAdapter
+{
+    [ExtensionUri(ProtractorTestExecutor.ExecutorUriString)]
+    public class ProtractorTestExecutor : ITestExecutor
+    {
+        #region Constants
+
+        /// <summary>
+        /// The Uri used to identify the XmlTestExecutor.
+        /// </summary>
+        public const string ExecutorUriString = "executor://ProtractorTestExecutor";
+
+        /// <summary>
+        /// The Uri used to identify the XmlTestExecutor.
+        /// </summary>
+        public static readonly Uri ExecutorUri = new Uri(ProtractorTestExecutor.ExecutorUriString);
+
+        /// <summary>
+        /// specifies whether execution is cancelled or not
+        /// </summary>
+        private bool m_cancelled;
+
+        #endregion
+
+        #region ITestExecutor
+
+        /// <summary>
+        /// Runs the tests.
+        /// </summary>
+        /// <param name="sources">Path to files to look for tests in.</param>
+        /// <param name="runContext">Context to use when executing the tests.</param>
+        /// <param param name="frameworkHandle">Handle to the framework to record results and to do framework operations.</param>
+        public void RunTests(IEnumerable<string> sources, IRunContext runContext, IFrameworkHandle frameworkHandle)
+        {
+            IEnumerable<TestCase> tests = ProtractorTestDiscoverer.GetTests(sources, null);
+
+            RunTests(tests, runContext, frameworkHandle);
+
+        }
+        /// <summary>
+        /// Runs the tests.
+        /// </summary>
+        /// <param name="tests">Tests to be run.</param>
+        /// <param name="runContext">Context to use when executing the tests.</param>
+        /// <param param name="frameworkHandle">Handle to the framework to record results and to do framework operations.</param>
+        public void RunTests(IEnumerable<TestCase> tests, IRunContext runContext, IFrameworkHandle frameworkHandle)
+        {
+            m_cancelled = false;
+
+            foreach (TestCase test in tests)
+            {
+                if (m_cancelled)
+                {
+                    break;
+                }
+
+                // Setup the test result as indicated by the test case.
+                var testResult = new TestResult(test)
+                {
+                    Outcome = (TestOutcome)test.GetPropertyValue(TestResultProperties.Outcome),
+                    ErrorMessage = (string)test.GetPropertyValue(TestResultProperties.ErrorMessage),
+                    ErrorStackTrace = (string)test.GetPropertyValue(TestResultProperties.ErrorStackTrace)
+                };
+
+                frameworkHandle.RecordResult(testResult);
+            }
+        }
+
+        /// <summary>
+        /// Cancel the execution of the tests
+        /// </summary>
+        public void Cancel()
+        {
+            m_cancelled = true;
+        }
+
+        #endregion
+
+    }
+
+}
