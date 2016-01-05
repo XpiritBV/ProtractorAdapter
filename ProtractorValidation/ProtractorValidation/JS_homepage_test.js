d97587bb6f812d9b69a7be11f055845baf747927
Marcel de Vries <vriesmarcel@hotmail.com>
2016-01-05 14:01:42 +0100

    remove additional describe

--- ProtractorValidation/ProtractorValidation/JS_homepage_test.js
+++ ProtractorValidation/ProtractorValidation/JS_homepage_test.js
@@ -10,17 +10,3 @@
 	});  
 });
 
-describe('angularjs homepage 2', function () {
-    it('should greet the named user', function () {
-        browser.get('http://www.angularjs.org');
-
-        element(by.model('yourName')).sendKeys('Julie');
-
-        var greeting = element(by.binding('yourName'));
-
-        expect(greeting.getText()).toEqual('Hello Julie!');
-    });
-});
-
-
-
